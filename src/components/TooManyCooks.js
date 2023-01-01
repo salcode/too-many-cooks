5fbb99acf3f182515e212ff5b651b90619aa6cce
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-01 17:16:06 -0500

    Extend TooManyCooks to use reducer

--- src/components/TooManyCooks.js
+++ src/components/TooManyCooks.js
@@ -1,9 +1,24 @@
+import { Button } from '@wordpress/components';
+import { useReducer } from '@wordpress/element';
+
+import reducer from '../reducer';
+
 export default function TooManyCooks({
   currentValue,
   editedValue,
 }) {
+  const [state, dispatch] = useReducer(
+    reducer,
+    {
+      databaseValue: false,
+    },
+  );
   return <>
     <h2>Too Many Cooks</h2>
-    {currentValue} / {editedValue}
+    {state.databaseValue ? 'true' : 'false'} / {currentValue} / {editedValue}
+    <Button onClick={() => dispatch({
+      type: 'setDatabaseValue',
+      value: ! state.databaseValue,
+    })}>Toggle state</Button>
   </>
 }
