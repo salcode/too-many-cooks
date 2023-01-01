5319c13476f8ab68a812f9366224ea7bfdf40509
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-01 17:56:11 -0500

    Build out TooManyCooks component

--- src/components/TooManyCooks.js
+++ src/components/TooManyCooks.js
@@ -1,4 +1,8 @@
 import { Button } from '@wordpress/components';
+import {
+  dispatch as wpDispatch,
+  select
+} from '@wordpress/data';
 import { useReducer } from '@wordpress/element';
 
 import reducer from '../reducer';
@@ -15,10 +19,55 @@ export default function TooManyCooks({
   );
   return <>
     <h2>Too Many Cooks</h2>
-    {state.databaseValue ? 'true' : 'false'} / {currentValue} / {editedValue}
-    <Button onClick={() => dispatch({
-      type: 'setDatabaseValue',
-      value: ! state.databaseValue,
-    })}>Toggle state</Button>
-  </>
+    {state.databaseValue ? 'true' : 'false'}/
+    {currentValue ? 'true' : 'false'}/
+    {editedValue ? 'true' : 'false'}
+    <ul>
+      <li key="database">
+        { state.databaseValue ? 'true' : 'false' }: 
+        Database Value
+        {state.isUpdatingDatabase ? 'updating...' : ''}
+        <Button className="is-primary" onClick={
+          async () => {
+            // Starting update.
+            dispatch({
+               type: 'setUpdatingDatabase',
+               value: true,
+            });
+            const newDatabaseValue = (await wp.apiFetch({
+              data: {
+                meta: { salcode_is_oven_on: ! state.databaseValue },
+              },
+              method: 'POST',
+              path: `/wp/v2/posts/${select('core/editor').getCurrentPostId()}`,
+            }))?.meta?.salcode_is_oven_on;
+
+            dispatch({
+              type: 'setDatabaseValue',
+              value: newDatabaseValue,
+            });
+
+            // Done updated.
+            dispatch({
+               type: 'setUpdatingDatabase',
+               value: false,
+            });
+          }
+        }>Toggle</Button>
+      </li>
+      <li key="currentValue">
+        {currentValue ? 'true' : 'false'}: 
+        Current (Gutenberg)
+      </li>
+      <li key="editedValue">
+        {editedValue ? 'true' : 'false'}: 
+        Edited (Gutenberg)
+        <Button className="is-primary" onClick={() =>
+           wpDispatch('core/editor').editPost({
+             meta: {salcode_is_oven_on: ! editedValue},
+          })
+        }>Toggle</Button>
+      </li>
+    </ul>
+  </>;
 }
