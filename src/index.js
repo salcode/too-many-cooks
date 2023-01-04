984b4909e8a668f19bae2b93f2a235deded2816d
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-03 20:53:38 -0500

    Remove unused from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import apiFetch from '@wordpress/api-fetch';
 import { registerPlugin } from '@wordpress/plugins';
 import {
-  dispatch,
   select,
   useSelect,
 } from '@wordpress/data';
@@ -9,69 +8,13 @@ import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import WarningMessage from './components/WarningMessage';
-import Value from './components/Value';
 
 import TooManyCooks from './components/TooManyCooks';
-
-const setGutenbergOvenTemp = (newOvenTemp) => {
-  dispatch('core/editor')
-    .editPost(
-      {
-        meta: {
-          salcode_oven_temp: newOvenTemp
-        }
-      }
-    );
-};
 
 registerPlugin(
   'too-many-cooks', // Unique identifier for our plugin.
   {
     render: () => {
-      const postMetaOvenTemp = select('core/editor')
-        .getCurrentPostAttribute('meta')
-        .salcode_oven_temp;
-      const dbValueOnLoad = postMetaOvenTemp === '' ? 0 : postMetaOvenTemp;
-
-      const [
-        dbValue,
-        setDbValue,
-      ] = useState(dbValueOnLoad);
-      const [
-        isUpdating,
-        setIsUpdating,
-      ] = useState(false);
-
-      const fetchDatabaseOvenTemp = async () => {
-        setIsUpdating(true);
-        const ovenTemp = (await apiFetch({
-          path: `/wp/v2/posts/${select('core/editor').getCurrentPostId()}`,
-        }))?.meta?.salcode_oven_temp;
-        setIsUpdating(false);
-        setDbValue(ovenTemp);
-      };
-
-      const setDatabaseOvenTemp = async (newOvenTemp) => {
-        setIsUpdating(true);
-        const updatedOvenTemp = (await wp.apiFetch({
-          data: {
-            meta: { salcode_oven_temp: newOvenTemp },
-          },
-          method: 'POST',
-          path: `/wp/v2/posts/${select('core/editor').getCurrentPostId()}`,
-        }))?.meta?.salcode_oven_temp;
-        setDbValue(updatedOvenTemp);
-        setIsUpdating(false);
-      }
-
-      const modifiedValue = useSelect(
-        (select) => {
-          return select('core/editor')
-            .getEditedPostAttribute('meta')
-            .salcode_oven_temp;
-        }
-      );
-
       const currentValue = useSelect(
         (select) => select('core/editor')
           .getCurrentPostAttribute('meta')
@@ -85,16 +28,11 @@ registerPlugin(
 
       return (
         <PluginDocumentSettingPanel
-          title={__('Oven Temp', 'too-many-cooks')}
+          title={__('Oven is On', 'too-many-cooks')}
         >
           <TooManyCooks
             currentValue={currentValue}
             editedValue={editedValue}
-          />
-          <WarningMessage
-            dbValue={dbValue}
-            dbValueOnLoad={dbValueOnLoad}
-            modifiedValue={modifiedValue}
           />
         </PluginDocumentSettingPanel>
       );
