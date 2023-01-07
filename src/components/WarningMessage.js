7268c96e78a6fe187eebb1168e1b2a6564483200
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-01-07 18:51:10 -0500

    Modify WarningMessage component

--- src/components/WarningMessage.js
+++ src/components/WarningMessage.js
@@ -1,30 +1,15 @@
 export default function WarningMessage({
-  dbValue,
-  dbValueOnLoad,
-  modifiedValue,
+  currentValue,
+  databaseValue,
+  editedValue,
 }) {
-  if (dbValue === dbValueOnLoad) {
-    // The database value matches the value Gutenberg thinks is in the database.
-    if (dbValueOnLoad !== modifiedValue) {
-      return <p className="components-notice is-success">{`Saving will set all values to ${modifiedValue}`}</p>;
-    }
-    return null;
+  if (currentValue !== editedValue) {
+    return <p className="components-notice is-success">{`Gutenberg has modified the value since it was last loaded. Saving will set all values to the editedValue in Gutenberg (${editedValue})`}</p>;
   }
-  if (dbValue === modifiedValue) {
-      return <p className="components-notice is-success">{`Saving will set all values to ${modifiedValue}`}</p>;
-  }
-  const dbGutenbergMismatchMessage = `The database value has been changed to ${dbValue} but Gutenberg is unaware of this change and thinks the database still has a value of ${dbValueOnLoad}.`;
 
-  if (dbValueOnLoad !== modifiedValue) {
-    return <>
-      <p className="components-notice is-warning">{`Saving will set all values to ${modifiedValue}`}</p>
-      <p className="components-notice">{dbGutenbergMismatchMessage}</p>
-      <p className="components-notice">{`If you save now, Gutenberg will prioritize the modified Gutenberg value (${modifiedValue}) and overwrite the database with that value.`}</p>
-    </>;
+  if (databaseValue !== currentValue) {
+    return <p className="components-notice is-success">{`Gutenberg has not modified the value. Saving will set all values to the Database value (${databaseValue})`}</p>;
   }
-  return <>
-      <p className="components-notice is-error">{`Saving will set all values to ${dbValue}`}</p>
-      <p className="components-notice">{dbGutenbergMismatchMessage}</p>
-      <p className="components-notice">{`Gutenberg will prioritize the database value (${dbValue}) because Gutenberg thinks the value in Gutenberg (${modifiedValue}) is unmodified. This works well most of the time, but if you actually want to set the value in the database to ${modifiedValue} you are out of luck.`}</p>
-  </>;
+
+  return null;
 }
